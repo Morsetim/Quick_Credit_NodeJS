60eb0d10ebb47fa542dac765e3c12e26751db9ba
Mors Etim <maur@gmail.com>
2021-06-08 14:28:33 +0100

    db-strings

--- server/models/db.js
+++ server/models/db.js
@@ -1,17 +1,17 @@
 import {Pool} from 'pg';
-import dotenv from 'dotenv';
-import configuration from '../config/config';
+// import dotenv from 'dotenv';
+// import configuration from '../config/config';
 
-const env = process.env.NODE_ENV || 'production';
-console.log(env, '===============env');
+// const env = process.env.NODE_ENV || 'production';
+// console.log(env, '===============env');
 
-const config = configuration[env];
-console.log(config, '-------cofig');
-const connectionString = config.url;
-console.log(connectionString, '========09090')
-dotenv.config();
+// const config = configuration[env];
+// console.log(config, '-------cofig');
+// const connectionString = config.url;
+// console.log(connectionString, '========09090')
+// dotenv.config();
 
-const db = new Pool({connectionString});
+const db = new Pool({connectionString: 'postgres://newnkymy:DcRPimLCOcd-IbU6Idu2o21JQDaIDpDq@isilo.db.elephantsql.com/newnkymy'});
 db.connect().then(() =>{
     console.log('Successfully connected to PostgresDB');
     console.log(connectionString, "==========Connected=========");
